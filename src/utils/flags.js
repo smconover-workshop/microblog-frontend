f9eee85bf2157d37d50d8c47fb39d347951e39fe
smconover <68867205+smconover@users.noreply.github.com>
2020-07-28 14:26:43 -0400

    Add impressionHandler

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -7,19 +7,30 @@ export const Flags = {
   title: new Rox.Flag(false)
 };
 
-
 export const configurationFetchedHandler = fetcherResults => {
   console.log('The configuration status is: ' + fetcherResults.fetcherStatus)
   if (fetcherResults.hasChanges && fetcherResults.fetcherStatus === 'APPLIED_FROM_NETWORK') {
     window.location.reload(false)
   }
   else if (fetcherResults.fetcherStatus === 'ERROR_FETCH_FAILED') {
+    export const impressionHandler = (reporting, experiment) => {
+      if (experiment.name === 'title') {
+        console.log('Title flag, ' + reporting.name + ' ,value is ' + reporting.value)
+        gtag('event', experiment.name, {
+          'event_category': reporting.name,
+          'event_label': reporting.value
+        })
+      } else {
+        console.log('Not in title experiment. Flag ' + reporting.name + '. default value ' + reporting.value + ' was used')
+      }
+    };
     console.log('Error occured! Details are: ' + fetcherResults.errorDetails)
   }
 };
 
 const options = {
   configurationFetchedHandler: configurationFetchedHandler
+  impressionHandler: impressionHandler
 };
 
 Rox.setCustomBooleanProperty('isLoggedIn', store.getters.isLoggedIn);
